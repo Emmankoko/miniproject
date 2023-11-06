a49677467b3781f930e779eb28ceedd5794b7610
Emmankoko <57257441+Emmankoko@users.noreply.github.com>
2023-11-06 13:24:12 +0000

    Add files via upload

--- visualization stock/app.py
+++ visualization stock/app.py
@@ -15,20 +15,20 @@ user_input = st.text_input('Enter Stock Ticker', 'AAPL')
 df = data.DataReader(user_input, 'yahoo', start, end)
 
 #Describing data
-st.subheader('Data from 2010 - 2019')
+st.subheader('Data from 2010 - 2022')
 st.write(df.describe())
 
 #visualization
 st.subheader('Closing Price vs Item Chart')
 fig = plt.figure(figsize = (12,6))
-plt.plot(df.close)
+plt.plot(df.Close)
 st.pyplot(fig)
 
 st.subheader('Closing Price vs Item Chart with 100MA')
 ma100 = df.Close.rolling(100).mean()
 fig = plt.figure(figsize = (12,6))
 plt.plot(ma100)
-plt.plot(df.close)
+plt.plot(df.Close)
 st.pyplot(fig)
 
 st.subheader('Closing Price vs Item Chart with 100MA & 200MA')
@@ -37,11 +37,11 @@ ma200 = df.Close.rolling(200).mean()
 fig = plt.figure(figsize = (12,6))
 plt.plot(ma100, 'r')
 plt.plot(ma200, 'g')
-plt.plot(df.close, 'b')
+plt.plot(df.Close, 'b')
 st.pyplot(fig)
 
 data_training = pd.DataFrame(df['Close'][0: int(len(df) * 0.70) ])
-data_testing = pd.DataFrame(df['Close '][int (len(df) * 0.70) : int(len(df))])
+data_testing = pd.DataFrame(df['Close'][int (len(df) * 0.70) : int(len(df))])
 
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler(feature_range = (0,1))
@@ -50,7 +50,7 @@ data_training_array = scaler.fit_transform(data_training)
 
 
 
-model = load_model('keras-model.h5')
+model = load_model('keras_model.h5')
 
 past_100_days = data_training.tail(100)
 final_df = past_100_days.append(data_testing, ignore_index = True)
@@ -73,14 +73,14 @@ y_test = y_test * scale_factor
 
 #final graph
 
-st.Subheader('Prediction VS Original')
+st.subheader('Prediction VS Original')
 
-plt.figure(figsize=(12,6))
+fig2 = plt.figure(figsize=(12,6))
 plt.plot(y_test, 'b', label = 'Original Price')
 plt.plot(y_predicted, 'r', label = 'Predicted Price')
 plt.xlabel('Time')
 plt.ylabel('Price')
 plt.legend()
-plt.show()
+st.pyplot(fig2)
 
 
